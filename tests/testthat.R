355d12b49e0f51cf17278cc0de888a3817cd29b7
Simon_Staehli <simon.staehli@students.fhnw.ch>
2022-03-02 11:18:48 +0100

    Added new funcs to worklfow

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,11 +1,1 @@
 library(testthat)
-library(knitr)
-
-runAllChunks <- function(rmd, envir=globalenv()){
-  tempR <- tempfile(tmpdir = ".", fileext = ".R")
-  on.exit(unlink(tempR))
-  knitr::purl(rmd, output=tempR)
-  testthat::test_file(tempR)
-}
-
-runAllChunks("Mini-Challenge_2.Rmd")
