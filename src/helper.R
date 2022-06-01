fe2154e5cae10893149e1f3f5b43592e4aa17c4b
kenfus <vincenzo.timmel@students.fhnw.ch>
2022-06-01 14:25:51 +0200

    Fertig!

--- src/helper.R
+++ src/helper.R
@@ -10,8 +10,6 @@ library("gridExtra")
 library("fmsb")
 
 
-
-
 #' turn_into_wide
 #' Turns a long dataframe into wide dataframe
 #'
@@ -91,5 +89,81 @@ get_topn_recos <- function(df,n){
   return(df11)
 }
 
+#' Function returns top n recommendations.
+#'
+#' @param user_profiles dataframe containing the number of genres of movies of the ground truth and our recommendations.
+#'
+#' @return cleveland-plot with ground_truth of genres vs genres of our recommendations.
+create_cleveland_plot <- function(user_profiles){
+  # Plot cleveland plot with relevant difference percentage of prediced movies
+  for (u_id in unique(user_profiles$user)){
+    user_profile_select <- user_profiles[user_profiles$user == u_id, ] %>%
+      select(-c(user)) 
+    
+    rownames(user_profile_select) <- user_profile_select$ground_truth 
+    user_profile_select <- t(user_profile_select)
+    user_profile_select <- head(user_profile_select, dim(user_profile_select)[[1]] - 1) %>%
+      as.data.frame()
+    colnames(user_profile_select) <- c("ground_truth", "recommendation")
+    
+    user_profile_select$ground_truth <- user_profile_select$ground_truth / sum(user_profile_select$ground_truth) 
+    user_profile_select$recommendation <- user_profile_select$recommendation / sum(user_profile_select$recommendation) 
+    user_profile_select$genre <- rownames(user_profile_select)
+    rownames(user_profile_select) <- NULL
+    
+    # Plot
+    p <- ggplot(user_profile_select) +
+      geom_segment( aes(x=genre, xend=genre, y=ground_truth, yend=recommendation), color="grey") +
+      geom_point( aes(x=genre, y=ground_truth), color=rgb(0.2,0.7,0.1,0.5), size=3 ) +
+      geom_point( aes(x=genre, y=recommendation), color=rgb(0.7,0.2,0.1,0.5), size=3) +
+      coord_flip()+
+      xlab("") +
+      ylab("Percentage recommended vs. watched genre") + 
+      labs(title = paste("Recommendation vs Ground Truth for user", u_id))
+    print(p)
+    }
+  }
 
 
+#' Function returns top n recommendations.
+#'
+#' @param selected_users melted similarity matrix of selected users
+#' @param n n for the Top n list
+#'
+#' @return cleveland-plot with ground_truth of genres vs genres of our recommendations for the selected users.
+#' 
+analyze_topn_recos <- function(selected_users, n){
+  # Extract recommended user profiles
+  top_n_recos = get_topn_recos(selected_users, n)
+  
+  colnames(top_n_recos) <- c("title", "user", "similarity")
+  top_n_recos$title <- as.character(top_n_recos$title)
+  
+  top_n_recos <- merge(x = top_n_recos, y = movie_genre, 
+                       by.x = "title", by.y = "title") %>%
+    select(-c(title, similarity))
+  
+  selected_cols <- top_n_recos %>%
+    select(-user) %>%
+    colnames()
+  
+  recommended_user_profiles <- top_n_recos %>%
+    group_by(user) %>%
+    summarise(across(everything(), sum))
+  recommended_user_profiles$ground_truth <- FALSE
+  
+  
+  # Extract true user profiles
+  tmp <- user_movie_mat %*% movie_genre_mat
+  tmp <- as.data.frame(tmp)
+  tmp$user <- rownames(tmp)
+  
+  # filter users
+  true_user_profiles <- tmp[tmp$user %in% c(recommended_user_profiles$user), ]
+  true_user_profiles$ground_truth <- TRUE
+  true_user_profiles
+  
+  
+  user_profiles <- rbind(true_user_profiles, recommended_user_profiles)
+  return(user_profiles)
+}
